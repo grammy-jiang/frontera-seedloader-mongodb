9e1f57ff018b1d3c91db8aa8eeda1bcd541fc836
Grammy Jiang <grammy.jiang@gmail.com>
2017-11-09 11:17:18 +0800

    initialize this project

---
+++ frontera_seedloader_mongodb/contrib/scrapy/middlewares/seeds/mongodb.py
@@ -0,0 +1,5 @@
+from frontera.contrib.scrapy.middlewares.seeds import SeedLoader
+
+
+class MongoDBAsyncSeedLoader(SeedLoader):
+    pass
---
+++ tests/test_contrib_scrapy_middlewares_seeds_mongodb.py
@@ -0,0 +1,7 @@
+from twisted.trial.unittest import TestCase
+
+from frontera_seedloader_mongodb.contrib.scrapy.middlewares.seeds.mongodb import MongoDBAsyncSeedLoader
+
+
+class TestMongoDBAsyncSeedLoader(TestCase):
+    pass
